f40d16b99efb220968be052d03e526ce2ffaec20
Rhommel Lamas <roml@rhommell.com>
2014-10-18 13:10:46 +0200

    Add better instance control

--- launch_ami.sh
+++ launch_ami.sh
@@ -2,36 +2,42 @@
 #
 # USAGE: launch_ami
 
-AMI_ID=ami-xxxx
-KEY_ID=xxxxxx
-INSTANCE_TYPE="t1.micro"
+NAME=instance01
+ENVIRONMENT=production
+AMI_ID=ami-ae62d7c6
+KERNEL_ID=aki-919dcaf8
+KEY_ID=3scale2014
+INSTANCE_TYPE='m3.large'
 EC2_RUN_INSTANCES=`which ec2-run-instances`
 EC2_DESCRIBE_INSTANCES=`which ec2-describe-instances`
-SEC_ID="xxx-xxx"
-AMAZON_AZ="eu-west-1a"
-SIZE=10
+EC2_CREATE_TAGS=`which ec2-create-tags`
+SEC_ID='backend-listener'
+AMAZON_AZ='us-east-1b'
+USER_DATA='instance01-cloudinit.txt'
 
 echo "Starting Instance..."
-INSTANCE_DETAILS=`${EC2_RUN_INSTANCES} -k ${KEY_ID} --block-device-mapping /dev/sda1=:${SIZE} -t ${INSTANCE_TYPE} -g ${SEC_ID} -z ${AMAZON_AZ} ${AMI_ID} --disable-api-termination`
-echo ${INSTANCE_DETAILS}
+INSTANCE_DETAILS=`${EC2_RUN_INSTANCES} -k ${KEY_ID} -t ${INSTANCE_TYPE} -g ${SEC_ID} -z ${AMAZON_AZ} ${AMI_ID} --kernel ${KERNEL_ID} -f ${USER_DATA} --disable-api-termination`
 
-AVAILABILITY_ZONE=`echo ${INSTANCE_DETAILS} | awk '{print $13}'`
+AVAILABILITY_ZONE=`echo ${INSTANCE_DETAILS} | awk '{print $9}'`
 INSTANCE_ID=`echo ${INSTANCE_DETAILS} | awk '{print $6}'`
 
-echo "Availability Zone ${AVAILABILITY_ZONE}"
-echo "Instance ID ${INSTANCE_ID}"
+# Set tags
+
+echo "Creating tags"
+${EC2_CREATE_TAGS} ${INSTANCE_ID} --tag "Name=${NAME}" --tag "Environment=${ENVIRONMENT}"
 
 # wait for instance to be started
-DNS_NAME=`${EC2_DESCRIBE_INSTANCES} --filter "image-id=$AMI_ID" --filter "instance-state-name=running" --filter "instance-id=${INSTANCE_ID}" | grep INSTANCE | awk '{print $4}'`
+DNS_NAME=`${EC2_DESCRIBE_INSTANCES} --filter "tag-key=Name" --filter "tag-value=${NAME}" --filter "instance-state-name=running" | grep INSTANCE | awk '{print $4}'`
 
 while [ -z "$DNS_NAME" ]
 do
  echo "Waiting for instance to start...."
  sleep 5
- DNS_NAME=`${EC2_DESCRIBE_INSTANCES} --filter "image-id=$AMI_ID" --filter "instance-state-name=running" --filter "instance-id=${INSTANCE_ID}" | grep INSTANCE | awk '{print $4}'`
+ DNS_NAME=`${EC2_DESCRIBE_INSTANCES} --filter "tag-key=Name" --filter "tag-value=${NAME}" --filter "instance-state-name=running" | grep INSTANCE | awk '{print $4}'`
 done
 
 echo "Instance started"
 
-echo "Instance ID = ${INSTANCE_ID}"
-echo "DNS = ${DNS_NAME} in availability zone ${AVAILABILITY_ZONE}"
+echo "Instance ID  => ${INSTANCE_ID}"
+echo "DNS          => ${DNS_NAME}"
+echo "AVAILABILITY => ${AVAILABILITY_ZONE}"
